bcd4d6ad90f4a92933edf3fc3841e10a30a704ba
Christoph33one <cjbrowning23@hotmail.com>
2023-03-26 12:48:01 +0000

    used the API data and rendered it as an object and an array. used Http error handling method

--- my-app/src/components/HTTPRequests.js
+++ my-app/src/components/HTTPRequests.js
@@ -6,26 +6,52 @@ export class HTTPRequests extends Component {
       super(props)
     
       this.state = {
-         posts: []
+         posts: [],
+         error: null
       }
     }
     // using the axios fetch url.
+    // line 20 - 22 - to post one item in the data array
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
         .then(response => {
             console.log(response)
             // call setState to update the componet in the state
             this.setState({
-                posts: response.data
+                posts: Array.isArray(response.data)
+                ? response.data
+                :[response.data]
+            })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                })
             })
         })
     }
   render() {
+    const posts = this.state.posts;
     return (
         // render the data (JSON)
+        // rendering data as a list 
       <div>
         <h2>Posts:</h2>
-        {JSON.stringify(this.state.posts)}
+        {
+            posts.length ?  (
+                posts.map(post => (
+                    <div key={post.di}>
+                        <h2>{post.id}. {post.title}</h2>
+                        <h4>User id{post.userId}</h4>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>
+                ))
+                ) : ( // render an error of post is not found using the catch method - line 25
+                    this.state.error
+                    ? <p>{this.state.error}</p>
+                    : <h4>Loading posts ...</h4>
+                )
+        }
       </div>
     )
   }
